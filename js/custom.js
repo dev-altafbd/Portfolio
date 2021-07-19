78f691202eeffda3dcbfb81cbe2585d5d6040243
dev-altafbd <78550237+dev-altafbd@users.noreply.github.com>
2021-07-19 13:31:11 +0600

    update

--- js/custom.js
+++ js/custom.js
@@ -13,6 +13,35 @@ $(window).on('load', function() {
         document.getElementsByClassName('infinity')[0].style.display = "block";
     }
 
+      // Hero typed
+      if ($('.typed').length) {
+        var typed_strings = $(".typed").data('typed-items');
+        typed_strings = typed_strings.split(',')
+        new Typed('.typed', {
+          strings: typed_strings,
+          loop: true,
+          typeSpeed: 100,
+          backSpeed: 50,
+          backDelay: 2000
+        });
+      };
+      //Review
+      $(".testimonials-carousel").owlCarousel({
+        autoplay: true,
+        dots: true,
+        loop: true,
+        responsive: {
+          0: {
+            items: 1
+          },
+          768: {
+            items: 2
+          },
+          900: {
+            items: 3
+          }
+        }
+      });
     /*=========================================================================
      Wow Initialize
      =========================================================================*/
